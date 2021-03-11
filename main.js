1953cef748035b6e80753fec848547aaffd9622c
whimsyee <noeymaiih@gmail.com>
2021-03-11 11:52:11 +0900

    main.js
    전역 변수 사용하지 않기 위해 익명함수를 바로 실행함
    1. html 내 원하는 element에 붙은 class를 가져온다.
    2. for문으로 data-index 를 붙여준다.
    3. dataset 읽기 전용 속성
    https://developer.mozilla.org/ko/docs/Web/API/HTMLOrForeignElement/dataset

---
+++ main.js
@@ -0,0 +1,13 @@
+// 전역 변수 사용 회피 => 즉시 사용 함수
+// 화살표 함수를 만들고 감싼 다음 가로 연산자로 실행을 함
+
+(() => {
+	const stepElems = document.querySelectorAll('.step');
+	const graphicElems = document.querySelectorAll('.graphic-item');
+
+	for (let i = 0; i < stepElems.length; i++) {
+		// stepElems[i].setAttribute('data-index', i);
+		stepElems[i].dataset.index = i;
+		graphicElems[i].dataset.index = i;
+	}
+})();
