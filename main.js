e6986645462921a088dd3c07a315f9a4692f5d8b
whimsyee <noeymaiih@gmail.com>
2021-03-11 15:52:10 +0900

    DOM 엘리먼트의 위치 구하기 윈도우 - 스크롤할때마다 발생하는 이벤트 step(말풍선 박스)의 위치 값을 구하는 방법 step.getBoundingClientRect(); Dom.getBoundingClientRect();

--- main.js
+++ main.js
@@ -10,4 +10,15 @@
 		stepElems[i].dataset.index = i;
 		graphicElems[i].dataset.index = i;
 	}
+
+	window.addEventListener('scroll', () => {
+		let step;
+		let boundingRect;
+
+		for (let i = 0; i < stepElems.length; i++) {
+			step = stepElems[i];
+			boundingRect = step.getBoundingClientRect();
+			console.log(boundingRect);
+		}
+	});
 })();
